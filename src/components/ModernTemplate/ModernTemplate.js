b9471cab56683c2c806e4bc27293043e277e8b4e
erdener <erdenerunale@gmail.com>
2022-02-20 18:38:00 +0300

    fonawesome implemented.

--- src/components/ModernTemplate/ModernTemplate.js
+++ src/components/ModernTemplate/ModernTemplate.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import './ModernTemplate.scss';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser, faLayerGroup, faBarsProgress, faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import React from "react";
+import "./ModernTemplate.scss";
 
 export const ModernTemplate = () => {
   return (
-        <div className='main container-fluid mx-auto' >
-            <div className="navbar">
-                <ul>
-                    <li><i class="fa-solid fa-user"></i></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                </ul>
-            </div>
+    <div className="main container-fluid mx-auto">
+      <div className="navbar justify-content-center">
+        <div>
+          <FontAwesomeIcon icon={faUser} />
         </div>
-    )
+        <div>
+          <FontAwesomeIcon icon={faLayerGroup} />
+        </div>
+        <div>
+          <FontAwesomeIcon icon={faBarsProgress} />
+        </div>
+        <div>
+          <FontAwesomeIcon icon={faEnvelope} />
+        </div>
+      </div>
+    </div>
+  );
 };
